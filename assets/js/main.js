efc02e3e2425e886a8df42d48ecc4df7ac16ba65
My-projects-dev <anar.ismayilov.1991@inbox.ru>
2026-01-29 19:28:38 +0400

    Modal and responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,8 @@ const search_input = document.getElementById("search-input");
 const total_products = document.getElementById("total_products");
 const loader = document.querySelector(".loader");
 const total_products_full_name = document.querySelector(".total_container p");
+const detail_section = document.querySelector("#detail_section");
+let data;
 
 load_btn.addEventListener("click", () => {
   fetchProducts(get_list);
@@ -19,10 +21,14 @@ clear_list.addEventListener("click", () => {
 });
 
 search_buttom.addEventListener("click", () => {
-  let data = encodeURIComponent(search_input.value.trim());
+  data = encodeURIComponent(search_input.value.trim());
   data = `${get_list}/search?q=${data}`;
   product_container.innerHTML = "";
   fetchProducts(data);
+});
+
+detail_section.addEventListener("click", function (event) {
+  if (event.target.id == "detail_section") closeModal();
 });
 
 function totalProducts(totalLength) {
@@ -55,11 +61,11 @@ function addProducts(products) {
   totalProducts(products.length);
 
   products.forEach((product) => {
-    product_container.innerHTML += `<div class="product">
+    product_container.innerHTML += `<div class="product" id="${product.id}" onclick="detailFetch(${product.id})">
           <figure class="product__img">
             <img
               src="${product.images[0]}"
-              alt=""
+              alt="${product.title}"
             />
           </figure>
           <span class="product__category">${product.category}</span>
@@ -71,4 +77,44 @@ function addProducts(products) {
           </div>
         </div>`;
   });
+}
+
+async function detailFetch(productId) {
+  try {
+    const productUrl = `${get_list}/${productId}`;
+    const productDetail = await fetch(productUrl);
+    if (productDetail.ok) {
+      const single_data = await productDetail.json();
+      showDetail(single_data);
+    } else {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
+
+function showDetail(product) {
+  detail_section.style.display = "block";
+  detail_section.innerHTML = `<div class="detail">
+        <div class="detail__header">
+        <i class="fa-regular fa-circle-xmark detail__header__close_modal" onclick="closeModal()">
+        </i></div>
+          <figure class="detail__img">
+            <img src="${product.images[0]}" alt="${product.title}" />
+          </figure>
+          <div class="detail__info">
+           <h1 class="detail__title">${product.title}</h1>
+            <span class="detail__category">${product.category}</span>
+            <p class="detail__description">${product.description}</p>
+            <div class="detail__footer">
+            <span class="detail__price">$${product.price}</span>
+            <span class="detail__raiting "><i class="fa-solid fa-star"></i> ${product.rating}</span>
+            </div>
+          </div>
+        </div>`;
+}
+
+function closeModal() {
+  detail_section.style.display = "none";
 }
